fabe120a5910200aed699eaa3acef427c6b2b751
den4k-pr <106625230+den4k-pr@users.noreply.github.com>
2023-09-13 07:02:13 +0200

    Update mongodb.ts

--- src/libs/mongodb.ts
+++ src/libs/mongodb.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectMongoDB = async () => {
   try {
-    await mongoose.connect("mongodb+srv://den4ikyakov:ZmyTAuTR77yZ4INz@cluster0.ranzlq2.mongodb.net/den4ikyakov");
+    await mongoose.connect("mongodb+srv://yakovden4k:biwpWZ9Spip6bGnj@cluster0.9yi7gcc.mongodb.net/yakovden4k");
     console.log("Connected to MongoDB.");
   } catch (error) {
     console.log(error);
